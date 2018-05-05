beaffff0f3e894006e7b9c5f56a1b7872857a48f
javi <javier.guinea.perez@alumnos.upm.es>
2018-05-05 18:42:26 +0200

    Actualizar dependencias y Readme

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ To do:
 *  BBDD
 *  Perfiles
 *  Creación de platos
+*  Generar CSVs con platos y recetas a partir de las recetas de Robin Food
 
 
 ## Créditos
@@ -22,3 +23,7 @@ https://www.w3schools.com/w3css
 Plantilla de react con webpack:
 
 https://github.com/KleoPetroff/react-webpack-boilerplate/blob/master/webpack.config.js
+
+Recetas:
+
+http://blog.daviddejorge.com
--- web_scraper/davidejorge/davidejorge/spiders/recetabot.py
+++ web_scraper/davidejorge/davidejorge/spiders/recetabot.py
@@ -2,17 +2,18 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from bs4 import BeautifulSoup
-	
+
 class RecetabotSpider(CrawlSpider):
     name = 'recetabot'
     allowed_domains = ['blog.daviddejorge.com/']
     start_urls = ['http://blog.daviddejorge.com/2016/07/22/recetas-expres-pollo-frito-guarripe/']
+    recetas = []
     def parse(self,response):
         soup = BeautifulSoup(response.text, 'lxml')
         articulo = soup.find('article')
         children = articulo.findChildren()
         lastChild = None
         for child in children:
-            print child
-            lastChild = child
+            if (child.name == "strong") or ((child.name == "h3") and (child['class']!=["sd-title"])):
+                self.recetas.append({'name': child.text})
         
